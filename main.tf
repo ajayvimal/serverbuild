06c2879c48049ee0aae17d59f0096812c4a1cee7
ajayvimal <57667382+ajayvimal@users.noreply.github.com>
2026-02-24 09:36:26 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,48 +1,45 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.36.0"
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {
+    api_management {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted         = false
+    }
+
+    application_insights {
+      disable_generated_rule = true
+    }
+
+    cognitive_account {
+      purge_soft_delete_on_destroy = true
+    }
+
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+
+    managed_disk {
+      expand_without_downtime = true
+    }
+
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = true
+    }
+
+    virtual_machine_scale_set {
+      force_delete                  = false
+      roll_instances_when_required  = true
+      scale_to_zero_before_deletion = true
     }
   }
-}
-
-provider "azurerm" {
-subscription_id = "dd2389996-6c64-43f2-8014-e5dff39e5e68"
-client_id = "2595c5a6-9cb2-41b1-a1f7-d917a52e5519"
-client_secret = "159f7c5a-d0d2-4474-8aa1-5f3d692169a2"
-tenant_id = "e39ad0e8-07a8-4895-857c-71c83191a3cf"
-
-features {
-  
-}
-}
-
-
-resource "azurerm_resource_group" "RG" {
-  name     = "MyRG"
-  location = "West Europe"
-}
-resource "azurerm_storage_account" "storage" {
-  name                     = "webstorage1251"
-  resource_group_name      = "MyRG"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "container1"
-  storage_account_id    = "/subscriptions/d2389996-6c64-43f2-8014-e5dff39e5e68/resourceGroups/MyRG/providers/Microsoft.Storage/storageAccounts/webstorage1251"
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "blob" {
-  name                   = "mynewblob"
-  storage_account_name   = "webstorage1251"
-  storage_container_name = "container1"
-  type                   = "Block"
-  source                 = "main.tf"
 }
